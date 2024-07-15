1fb1f80305d87c6ebcd1f59f1d118a84d19d8270
Mario Torres <scaramuzzini@gmail.com>
2024-07-15 11:16:34 -0400

    axios and starlink request

--- src/StarlinkList.js
+++ src/StarlinkList.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './StarlinkList.css';
+import axios from 'axios';
+
+function olaMundo() {
+}
+
 function StarlinkList() {
+
+    useEffect(() => { 
+        const fetchStarlinks = async () => {
+            const response = await axios.post('https://api.spacexdata.com/v4/starlink/query', {
+                "query": {},
+                "options": { limit: 10 }
+            });
+            console.log(response);
+        }
+        fetchStarlinks();
+    },[]);
+    
+
+
     //Meu componente starlink
     return (
         <>
