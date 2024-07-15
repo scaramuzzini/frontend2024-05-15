1a8865a4e55296d47baf5e058b5a9766765846a9
Mario Torres <scaramuzzini@gmail.com>
2024-07-15 10:44:40 -0400

    StarlinkList component

---
+++ src/StarlinkList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './StarlinkList.css';
+function StarlinkList() {
+    //Meu componente starlink
+    return (
+        <>
+            <h1>Satélites Starlink</h1>
+        </>
+    )
+}
+
+export default StarlinkList;
